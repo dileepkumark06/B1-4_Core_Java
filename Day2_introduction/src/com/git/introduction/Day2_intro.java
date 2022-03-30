5a0376df08a318e34ac39acd1f2d9b384180be49
sanju <sanju@DileepKumar>
2022-03-30 18:38:43 +0530

    First Commit

---
+++ Day2_introduction/src/com/git/introduction/Day2_intro.java
@@ -0,0 +1,14 @@
+package com.git.introduction;
+import java.util.Scanner;
+
+public class Day2_intro {
+
+	  public static void main(String[] args)
+	{
+		 Scanner s=new Scanner(System.in);
+		char c=s.next().charAt(4);
+		System.out.println(c);
+		s.close();
+	}
+	 
+}
