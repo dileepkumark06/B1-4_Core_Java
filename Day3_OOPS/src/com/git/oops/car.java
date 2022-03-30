66f561ce764de9cad07de203614630846dfb7e1f
sanju <sanju@DileepKumar>
2022-03-30 18:56:37 +0530

    First Commit

---
+++ Day3_OOPS/src/com/git/oops/car.java
@@ -0,0 +1,15 @@
+package com.git.oops;
+
+public class car 
+{
+void display()
+{
+	System.out.println("Welcome to C2TC program");
+}
+	public static void main(String[] args)
+	{
+		car c=new car();
+		c.display();
+	}
+}
+
