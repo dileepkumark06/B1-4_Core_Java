39cef48191664e22b0c898215fd08816c318bf24
sanju <sanju@DileepKumar>
2022-03-31 18:13:16 +0530

    First Commit

---
+++ Day4_packages/src/com/git/package1/Example1.java
@@ -0,0 +1,9 @@
+package com.git.package1;
+
+public class Example1 
+{
+	public void display()
+	{
+	System.out.println("welcome to concept of package");
+	}
+}
---
+++ Day4_packages/src/com/git/package2/Example2.java
@@ -0,0 +1,13 @@
+package com.git.package2;
+
+import com.git.package1.*;
+
+
+public class Example2 
+{
+	public static void main(String[] args) 
+	{
+		Example1 e=new Example1();
+		e.display();
+	}
+}
