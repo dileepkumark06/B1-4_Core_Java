30fa5473b2144ddf4b0bae2f6f2e5a97ae5efaa5
sanju <sanju@DileepKumar>
2022-04-05 00:31:20 +0530

    First Commit

---
+++ Assignment1/src/com/git/looping/assign2.java
@@ -0,0 +1,15 @@
+package com.git.looping;
+
+public class assign2 
+{
+	public static void main(String args[]) 
+	{
+		int arr[]= {12,13,14,44};
+		int total=0;
+		for(int i :arr)
+		{
+			System.out.println(i);
+		}
+
+	}
+}
