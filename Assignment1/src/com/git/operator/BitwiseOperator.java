541e76d1f1d17b95878d741741b1b07309f4c8aa
sanju <sanju@DileepKumar>
2022-04-04 16:21:57 +0530

    First Commit

---
+++ Assignment1/src/com/git/operator/BitwiseOperator.java
@@ -0,0 +1,16 @@
+package com.git.operator;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) 
+	{
+		int a=5,b=2;
+		int res=a & b;
+		int res1=a | b;
+		int res2=a ^ b;
+		System.out.println(res);
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+
+}
