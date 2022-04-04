69ef29f37dfe76c17fbab6ccf575feab749424cf
sanju <sanju@DileepKumar>
2022-04-05 00:02:03 +0530

    First Commit

---
+++ Assignment1/src/com/git/looping/Example1.java
@@ -0,0 +1,17 @@
+package com.git.looping;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			System.out.print(i+"  ");
+		}
+	}
+
+}
---
+++ Assignment1/src/com/git/operator/Unicode.java
@@ -0,0 +1,15 @@
+package com.git.operator;
+
+import java.util.Scanner;
+
+public class Unicode {
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		char c='\u002D';
+		System.out.println((int)c);
+		s.close();
+	}
+
+}
