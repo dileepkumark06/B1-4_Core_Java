15c3bb11d23688d9ba9944fe319e8bad70128fd2
sanju <sanju@DileepKumar>
2022-04-05 18:03:47 +0530

    First Commit

---
+++ Day7_this_static_instanceOf/src/com/git/this1/Example1.java
@@ -0,0 +1,16 @@
+package com.git.this1;
+
+public class Example1 
+{
+	String str;
+	Example1(String str)
+	{
+		this.str=str;
+	}
+	public static void main(String[] args)
+	{
+		Example1 e=new Example1("C2TC Program");
+		System.out.println(e.str);
+	}
+
+}
