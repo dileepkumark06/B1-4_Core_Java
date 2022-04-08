dc13f8647be8aa1a3e2c16b9cc7d1448b891eaf1
sanju <sanju@DileepKumar>
2022-04-08 18:54:35 +0530

    First Commit

--- Day10_Array/src/com/git/array/Example2.java
+++ Day10_Array/src/com/git/array/Example2.java
@@ -2,6 +2,12 @@ package com.git.array;
 
 public class Example2 
 {
-	int arr[]= {5,10,15,20};
-	for(int i=0)
+	public static void main(String[] args) 
+	{
+		int arr[]= {5,10,15,20};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
 }
