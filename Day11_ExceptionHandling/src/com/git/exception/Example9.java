23fdebf8410226e1f755de27e08ed68dc52a9c77
sanju <sanju@DileepKumar>
2022-04-12 12:09:50 +0530

    First Commit

---
+++ Day11_ExceptionHandling/src/com/git/exception/Example9.java
@@ -0,0 +1,28 @@
+package com.git.exception;
+
+import java.io.IOException;
+
+public class Example9 
+{
+	void myMethod (int num)throws IOException,ClassNotFoundException
+	{
+	   if (num==1)
+	      throw new IOException ("IOException Occurred");
+	   else
+	      throw new
+	ClassNotFoundException ("ClassNotFoundException");
+	}
+	public static void main (String args[])
+	{
+	 try
+	 {
+		 Example9 obj=new Example9() ;
+	      obj.myMethod(1);
+	 }
+	  catch (Exception ex)
+	 {
+	   System.out.println(ex);
+
+	 }
+	}
+}
