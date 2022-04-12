40014b6065209eeac6936fbbe3cc3d870e9dff3f
sanju <sanju@DileepKumar>
2022-04-12 12:00:25 +0530

    First Commit

--- Day11_ExceptionHandling/src/com/git/exception/Example4.java
+++ Day11_ExceptionHandling/src/com/git/exception/Example4.java
@@ -1,5 +1,22 @@
 package com.git.exception;
 
-public class Example4 {
-
+public class Example4 
+{
+	static void display(int age, int weight)
+	{
+		if(age>18 && weight>50)
+		{
+			
+			System.out.println("You are eligible");
+		}
+		else
+		{
+			throw new ArithmeticException("Not eligible");
+		}
+	}
+	public static void main(String[] args) 
+	{
+		display(16,55);
+		System.out.println("Exception Handled");
+	}
 }
