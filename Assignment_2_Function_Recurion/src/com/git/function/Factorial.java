577c01da71916d28c24ccdd36a8af98aed768cb2
sanju <sanju@DileepKumar>
2022-04-12 11:43:18 +0530

    First Commit

--- Assignment_2_Function_Recurion/src/com/git/function/Factorial.java
+++ Assignment_2_Function_Recurion/src/com/git/function/Factorial.java
@@ -15,7 +15,7 @@ public class Factorial
 		return fact;
 	}
 
-	public static void main(String[] args) 
+public static void main(String[] args) 
 	{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
