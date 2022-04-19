4db367815621d6b17fca31134cb7e457a60ed573
sanju <sanju@DileepKumar>
2022-04-19 18:23:34 +0530

    First Commit

--- Day16_JDBC/src/com/git/jdbc/DBConnect.java
+++ Day16_JDBC/src/com/git/jdbc/DBConnect.java
@@ -1,32 +1,48 @@
 package com.git.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBConnect 
 {
-
 	public static void main(String[] args) 
 	{
-String dbDriver="com.mysql.cj.jdbc.Driver";
-String dbURL="jdbc:mysql://localhost/Capgemini";
-String dbUserName="root";
-String dbPassword="dileep";
-String query="selct * from EnggStud"
-try
-{
-	Class.forName(dbDriver);
-}
-catch(ClassNotFoundException e)
-{
-	e.printstackTrace();
-}
-try
-{
-	Connection c=DriveManager.getConnection(dbURL,dbUserName,dbPassword);
-Statement st=con.createStatement();
-ResultSet rs=st.executeQuery(query);
-}
-while
+	String dbDriver="com.mysql.cj.jdbc.Driver";
+	String dbURL="jdbc:mysql://127.0.0.1:3306/Capgemini";
+	String dbUserName="root";
+	String dbPassword="dileep";
+	String query="select * from EnggStud";
+	try
+	{
+		Class.forName(dbDriver);
 	}
-
+	catch(ClassNotFoundException e)
+	{
+		e.printStackTrace();
+	}
+	try
+	{
+		Connection c=DriverManager.getConnection(dbURL,dbUserName,dbPassword);
+		Statement st=c.createStatement();
+		ResultSet rs=st.executeQuery(query);
+		
+	while(rs.next())
+	{
+		String Stud_data=" ";
+		for(int i=1;i<=2;i++)
+		{
+			Stud_data=Stud_data+" "+rs.getString(i);
+		}
+		System.out.println(Stud_data);
+	}
+  }
+catch(SQLException e)
+{
+	System.out.println("An error occured.Maybe password/user name is invalid");	
+	e.printStackTrace();
+}
+	}
 }
