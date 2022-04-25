d02b12054c257331fc93a13cd1c72104c6d135d4
sanju <sanju@DileepKumar>
2022-04-25 13:20:57 +0530

    First Commit

---
+++ Assignment3_Tree_Set_Map/src/com/git/collection/Example3.java
@@ -0,0 +1,17 @@
+package com.git.collection;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Example3 
+{
+	public static void main(String[] args) 
+	{
+		LinkedHashSet s=new LinkedHashSet();
+		s.add(11);
+		s.add(null);
+		s.add(11);
+		System.out.println(s);
+	}
+}
---
+++ Assignment3_Tree_Set_Map/src/com/git/collection/Example7.java
@@ -0,0 +1,5 @@
+package com.git.collection;
+
+public class Example7 {
+
+}
