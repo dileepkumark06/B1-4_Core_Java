dc8f27410192371f6aba25bffefcc5b88b259d23
sanju <sanju@DileepKumar>
2022-04-25 15:12:29 +0530

    First Commit

---
+++ Day19_JUnit_Testing/src/com/git/junittesting/JUnitTesting2.java
@@ -0,0 +1,23 @@
+package com.git.junittesting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTesting2
+{
+	@Test
+	void print()
+	{
+		int a=1;
+		int b=2;
+		assertEquals(a,b);
+	}
+	
+	@Test
+	void print1()
+	{
+		int a=1;
+		int b=1;
+		assertEquals(a,b);
+	}
+}
---
+++ Day19_JUnit_Testing/src/com/git/junittesting/JUnitTesting3.java
@@ -0,0 +1,25 @@
+package com.git.junittesting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTesting3 
+{
+	@Test
+	void print()
+	{
+		int a=1;
+		int b=1;
+		assertEquals(a,b);
+	}
+	
+	@Disabled
+	@Test
+	void print1()
+	{
+		int a=1;
+		int b=2;
+		assertEquals(a,b);
+	}
+}
