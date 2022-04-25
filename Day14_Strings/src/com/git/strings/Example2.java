ae675132a3f0feee0405bfd75d96f319118c3b91
sanju <sanju@DileepKumar>
2022-04-25 15:52:17 +0530

    First Commit

---
+++ Day14_Strings/src/com/git/strings/Example2.java
@@ -0,0 +1,14 @@
+package com.git.strings;
+
+import java.util.Scanner;
+
+public class Example2 
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		String str=new String(s.nextLine());
+		System.out.println(str);
+		s.close();
+	}
+}
---
+++ Day14_Strings/src/com/git/strings/Example6.java
@@ -0,0 +1,13 @@
+package com.git.strings;
+
+import java.util.StringTokenizer;  
+
+public class Example6 
+{
+	public static void main(String args[])
+	{  
+		StringTokenizer st = new StringTokenizer("Lets try this");  
+	    System.out.println(st.nextToken());  
+	    System.out.println(st.nextToken());
+	}  
+} 
