4af13ba9e828c31ab5f5fed631589801ab10a038
sanju <sanju@DileepKumar>
2022-04-27 19:27:32 +0530

    First Commit

--- Day22_Date_Time/src/com/git/datetime/Example1.java
+++ Day22_Date_Time/src/com/git/datetime/Example1.java
@@ -9,9 +9,13 @@ public class Example1
 {
 	public static void main(String[] args) 
 	{
+		LocalDate l =LocalDate.now();
+		LocalTime t=LocalTime.now();
 		LocalDateTime dt=LocalDateTime.now();
-		LocalDate d=LocalDate.of(2022,Month.APRIL,31);
+		LocalDate tt=LocalDate.of(2000,Month.FEBRUARY,29);
+		System.out.println(l);
+		System.out.println(t);
 		System.out.println(dt);
-		System.out.println(d);
+		System.out.println(tt);
 	}
 }
