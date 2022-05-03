8aa1d6b651a93b72143795d8cdbf30b06911a4c6
sanju <sanju@DileepKumar>
2022-05-03 18:01:40 +0530

    First Commit

---
+++ Day24_Java_Enum_and_REGEX/src/com/git/enum1/Example1.java
@@ -0,0 +1,14 @@
+package com.git.enum1;
+
+enum Car
+{
+	Mercedes,BMW,Audi
+}
+public class Example1 
+{
+	public static void main(String[] args) 
+	{
+		Car c=Car.BMW;
+		System.out.println(c);
+	}
+}
---
+++ Day24_Java_Enum_and_REGEX/src/com/git/enum1/Example2.java
@@ -0,0 +1,16 @@
+package com.git.enum1;
+
+public class Example2 
+{
+	enum Margin
+	{
+		TOP,BOTTOM,LEFT,RIGHT
+	}
+	public static void main(String[] args) 
+	{
+		int count=1;
+		for(Margin m:Margin.)
+
+	}
+
+}
